3eaced4d934464f1077e9dffc2df063df1f1de5c
Geoffroy Lesage <gefthefrench@gmail.com>
2016-12-07 15:20:37 -0600

    Added new link creation methods

--- branch.js
+++ branch.js
@@ -34,6 +34,7 @@ function request(path, method, parameters) {
  */
 var app = {
     create: function(data) {
+        if (!data) return Promise.reject(new Error('Missing data'));
         if (!data.user_id) return Promise.reject(new Error('Missing user_id'));
         if (!data.app_name) return Promise.reject(new Error('Missing app_name'));
         if (!data.dev_name) return Promise.reject(new Error('Missing dev_name'));
@@ -45,11 +46,64 @@ var app = {
             return Promise.reject(res);
         });
     },
-    update: function(data) {
-        if (!data.branch_key) return Promise.reject(new Error('Missing branch_key'));
+    update: function(branchKey, data) {
+        if (!data) return Promise.reject(new Error('Missing data'));
+        if (!branchKey) return Promise.reject(new Error('Missing branchKey'));
         if (!data.branch_secret) return Promise.reject(new Error('Missing branch_secret'));
 
-        var path = '/v1/app/' + data.branch_key;
+        var path = '/v1/app/' + branchKey;
+        return request(path, 'PUT', data);
+    }
+};
+
+/**
+ * Methods
+ */
+var link = {
+    findById: function(branchKey, url) {
+        if (!branchKey) return Promise.reject(new Error('Missing branchKey'));
+        if (!url) return Promise.reject(new Error('Missing url'));
+
+        var path = '/v1/url?url=' + url + "&branch_key=" + branchKey;
+        return request(path, 'GET').then(function(res) {
+            if (res) return res;
+            return Promise.reject(res);
+        });
+    },
+    create: function(branchKey, data) {
+        if (!branchKey) return Promise.reject(new Error('Missing branchKey'));
+
+        if (!data) data = {};
+        if (!data.type) data.type = 2;
+        if (!data.branch_key) data.branch_key = branchKey;
+
+        var path = '/v1/url';
+        return request(path, 'POST', data).then(function(res) {
+            if (res) return res;
+            return Promise.reject(res);
+        });
+    },
+    createMany: function(branchKey, data) {
+        if (!data) return Promise.reject(new Error('Missing data'));
+        if (!branchKey) return Promise.reject(new Error('Missing branchKey'));
+
+        data.forEach(function(link) {
+            if (!link.type) link.type = 2;
+        });
+
+        var path = '/v1/url/bulk/' + branchKey;
+        return request(path, 'POST', data).then(function(res) {
+            if (res) return res;
+            return Promise.reject(res);
+        });
+    },
+    update: function(branchKey, url, data) {
+        if (!branchKey) return Promise.reject(new Error('Missing branchKey'));
+        if (!url) return Promise.reject(new Error('Missing url'));
+        if (!data) return Promise.reject(new Error('Missing data'));
+        if (!data.branch_secret) return Promise.reject(new Error('Missing branch_secret'));
+
+        var path = '/v1/url/?url=' + url + "&branch_key=" + branchKey;
         return request(path, 'PUT', data);
     }
 };
@@ -58,3 +112,4 @@ var app = {
  * Exports
  */
 module.exports.app = app;
+module.exports.link = link;
